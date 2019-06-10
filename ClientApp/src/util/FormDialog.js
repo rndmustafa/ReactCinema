f6a305b428fabb224ec272f6a2a661adad0612db
Rand <rndmustafa@gmail.com>
2019-06-10 18:03:11 -0500

    Used rest syntax to make FormDialog more flexible.

--- ClientApp/src/util/FormDialog.js
+++ ClientApp/src/util/FormDialog.js
@@ -1,17 +1,15 @@
 ﻿import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 function FormDialog(props) {
-  const { open, setOpen, component: Component, itemName, handleItemAdd } = props;
+  const { open, setOpen, component: Component, ...rest } = props;
 
   return (
     <div>
       <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="new-item">
-        <DialogTitle id="form-dialog-title">New {itemName}</DialogTitle>
         <DialogContent>
-          <Component setOpen={setOpen} createMode={true} handleItemAdd={handleItemAdd} />
+          <Component setOpen={setOpen} {...rest} />
         </DialogContent>
       </Dialog>
     </div>
