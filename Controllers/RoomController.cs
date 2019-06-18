950a515ba003af450dbd652417517363b4d55ffe
Rand <rndmustafa@gmail.com>
2019-06-17 21:06:45 -0500

    Initial room controller

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactCinema.Data;
+using ReactCinema.Models;
+
+namespace ReactCinema.Controllers
+{
+    [Route("api/[controller]")]
+    public class RoomController : Controller
+    {
+        private readonly ReactCinemaDbContext _context;
+
+        public RoomController(ReactCinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRoomsAsync()
+        {
+            List<Room> rooms = await _context.Rooms.ToListAsync();
+            return Ok(rooms);
+        }
+    }
+}
