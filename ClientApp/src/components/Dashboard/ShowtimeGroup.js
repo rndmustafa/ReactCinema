2a991d5328e4b682d6448f44ca45af6db3010acc
Rand <rndmustafa@gmail.com>
2019-06-24 22:55:43 -0500

    Displaying ShowtimeGroups in the dashboard

---
+++ ClientApp/src/components/Dashboard/ShowtimeGroup.js
@@ -0,0 +1,80 @@
+﻿import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import moment from 'moment';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import ClearIcon from '@material-ui/icons/Clear';
+
+const style = {
+  container: {
+    padding: 5,
+    margin: '10px 0px 10px 0px'
+  },
+  blockContainer: {
+    display: 'flex',
+    flexWrap:'wrap'
+  },
+  block: {
+    width: 120,
+    border: '2px solid #e4e4e2',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '5px 5px 5px 5px',
+    marginRight: 8
+  },
+  flexRowSpace: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems:'center'
+  }
+};
+
+function ShowtimeGroup(props) {
+  const { groupData, classes, handleEditDialogOpen, handleDeleteDialogOpen } = props;
+
+  const formatHeaderDate = (date) => {
+    return moment(date).format('MMMM DD YYYY');
+  };
+
+  const formatTime = (time) => {
+    let timeArray = time.split(':');
+    let hour = parseInt(timeArray[0]);
+    let suffix = hour > 12 ? 'PM' : 'AM';
+    return `${hour%12}:${timeArray[1]} ${suffix}`;
+  };
+
+  let headerDate = formatHeaderDate(groupData.fromDate);
+  if (groupData.toDate !== groupData.fromDate) {
+    headerDate += ` - ${formatHeaderDate(groupData.toDate)}`;
+  }
+
+  return (
+    <div className={`${classes.container} ${classes.flexRowSpace}`}>
+      <div>
+        <Typography variant='h6'>{headerDate}</Typography>
+      <div className={classes.blockContainer}>
+        {groupData.showtimeGroupEntries.map(entry => (
+          <div key={entry.showtimeGroupEntryID} className={classes.block}>
+            <Typography variant='body1'>{formatTime(entry.startTime)}</Typography>
+            <div className={classes.flexRowSpace}>
+              <Typography variant='subtitle2'>Room {entry.room.title}</Typography>
+              <Typography variant='subtitle2'>{entry.experience.title}</Typography>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <IconButton onClick={() => handleEditDialogOpen(groupData)}>
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={() => handleDeleteDialogOpen(groupData)}>
+          <ClearIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default withStyles(style)(ShowtimeGroup);
--- ClientApp/src/components/Dashboard/ShowtimeList.js
+++ ClientApp/src/components/Dashboard/ShowtimeList.js
@@ -4,21 +4,75 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import FormDialog from '../../util/FormDialog';
 import ShowtimeForm from './ShowtimeForm';
+import ShowtimeGroup from './ShowtimeGroup';
+import WarnDeleteDialog from '../../util/WarnDeleteDialog';
 
 function ShowtimeList(props) {
-  const [formDialog, setFormDialog] = useState(false);
   const { movieID } = props;
+
+  const [formDialog, setFormDialog] = useState(false);
+  const [deleteDialog, setDeleteDialog] = useState(false);
+  const [selectedGroup, setSelectedGroup] = useState({});
+
+  const [groups, setGroups] = useState([]);
+  useEffect(() => {
+    fetch(`api/movie/${movieID}/showtimegroups`)
+      .then(res => res.json())
+      .then(resData => {
+        setGroups(resData);
+      });
+  }, []);
+
+  const handleDelete = () => {
+    setGroups(groups.filter((group) => group.showtimeGroupID !== selectedGroup.showtimeGroupID));
+  };
+
+  const handleDeleteDialogOpen = (group) => {
+    setSelectedGroup(group);
+    setDeleteDialog(true);
+  };
+
+  const handleEditDialogOpen = (group) => {
+    setSelectedGroup(group);
+    setFormDialog(true);
+  };
+
+  const handleGroupAdd = (newGroup) => {
+    setGroups(groups.concat(newGroup));
+  };
+
+  const handleGroupUpdate = (updatedGroup) => {
+    let index = groups.findIndex(group => group.showtimeGroupID === updatedGroup.showtimeGroupID);
+    let newGroups = [...groups];
+    newGroups[index] = updatedGroup;
+    setGroups(newGroups);
+  };
 
   return (
     <div>
-      <Button variant="contained" color="secondary" onClick={() => { setFormDialog(true); }}>
+      <Button style={{marginTop: 10}} variant="contained" color="secondary" onClick={() => { setFormDialog(true); }}>
         <AddIcon />Add Showtimes
       </Button>
+      <WarnDeleteDialog
+        open={deleteDialog}
+        setOpen={setDeleteDialog}
+        fetchUrl={`api/movie/showtimegroups/${selectedGroup.showtimeGroupID}`}
+        handleDelete={handleDelete} />
       <FormDialog
         open={formDialog}
         setOpen={setFormDialog}
         component={ShowtimeForm}
-        movieID={movieID} />
+        movieID={movieID}
+        handleItemAdd={handleGroupAdd}
+        handleItemUpdate={handleGroupUpdate}
+        groupData={Object.keys(selectedGroup).length === 0 ? null : selectedGroup} />
+      {groups.map(group => (
+        <ShowtimeGroup
+          key={group.showtimeGroupID}
+          groupData={group}
+          handleDeleteDialogOpen={handleDeleteDialogOpen}
+          handleEditDialogOpen={handleEditDialogOpen} />
+      ))}
     </div>
   );
 }
