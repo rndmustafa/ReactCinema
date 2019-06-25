9ea236023936c45d31574db9bc6d9139730b0deb
Rand <rndmustafa@gmail.com>
2019-06-24 22:54:18 -0500

    Created fetch functions in order to reduce redundancy

---
+++ ClientApp/src/util/fetchCalls.js
@@ -0,0 +1,36 @@
+﻿export function fetchCreateItem(endpoint,data,token) {
+  let requestInit = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(endpoint, requestInit)
+      .then(resp => resolve(resp))
+      .catch(err => reject(err));
+  });
+
+}
+
+export function fetchPutItem(endpoint, data, token) {
+  let requestInit = {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(endpoint, requestInit)
+      .then(resp => resolve(resp))
+      .catch(err => reject(err));
+  });
+}
