ec6d01d89665ef69f3c82820c560b7bf6be904c4
Rand <rndmustafa@gmail.com>
2019-07-23 23:06:17 -0500

    Initial reservation controller

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactCinema.Data;
+using ReactCinema.Models;
+
+namespace ReactCinema.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationController : Controller
+    {
+        private readonly ReactCinemaDbContext _context;
+
+        public ReservationController(ReactCinemaDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
