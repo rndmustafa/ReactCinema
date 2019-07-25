9bb46d447f667f4d8487eefadf9ec769490fc397
Rand <rndmustafa@gmail.com>
2019-07-24 20:53:54 -0500

    Backend logic for creating reservations

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -19,6 +19,61 @@ namespace ReactCinema.Controllers
             _context = context;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> PostReservationAsync(string email, int adultTickets, int childTickets, int showtimeID)
+        {
+            if(email == "")
+            {
+                return BadRequest(new { general = "Please enter your email" });
+            }
+            else if(adultTickets + childTickets == 0)
+            {
+                return BadRequest(new { general = "You must reserve at least one ticket" });
+            }
 
+            int reservationsMade = _context.Reservations
+                .Where(r => r.ShowtimeID == showtimeID)
+                .Count();
+            int roomCapacity = _context.Showtimes
+                .Where(s => s.ShowtimeID == showtimeID)
+                .Select(s => s.Room.Capacity)
+                .Single();
+
+            if(reservationsMade + adultTickets + childTickets > roomCapacity)
+            {
+                return BadRequest(new { general = "There aren't enough free seats to complete your reservation. " +
+                    "Please choose a different showtime." });
+            }
+
+            for(int i = 0; i < adultTickets; i++)
+            {
+                Reservation reservation = new Reservation
+                {
+                    ShowtimeID = showtimeID,
+                    Type = "Adult",
+                    Email = email
+                };
+                _context.Reservations.Add(reservation);
+            }
+            for (int i = 0; i < childTickets; i++)
+            {
+                Reservation reservation = new Reservation
+                {
+                    ShowtimeID = showtimeID,
+                    Type = "Child",
+                    Email = email
+                };
+                _context.Reservations.Add(reservation);
+            }
+
+            if (reservationsMade + adultTickets + childTickets == roomCapacity)
+            {
+                Showtime showtime = await _context.Showtimes.FindAsync(showtimeID);
+                showtime.Soldout = true;
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
