c544192e16e723ab52a63376978bd0730f9d9512
Rand <rndmustafa@gmail.com>
2019-07-24 23:09:52 -0500

    Wrapped dashboard elements in Paper

--- ClientApp/src/components/Dashboard/Dashboard.js
+++ ClientApp/src/components/Dashboard/Dashboard.js
@@ -14,58 +14,64 @@ import MovieIcon from '@material-ui/icons/Movie';
 import LocalPlayIcon from '@material-ui/icons/LocalPlay';
 import StarIcon from '@material-ui/icons/Star';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
-const style = {
+const style = (theme) => ({
   flexCol: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
-  }
-};
+  },
+  paper: theme.paper
+});
 
 function Dashboard(props) {
   const { classes, match } = props;
 
   return (
     <div className={classes.flexCol}>
-      <Grid container direction="row" justify="center" style={{ maxWidth: 1200 }}>
+      <Grid container direction="row" justify="center" spacing={24} style={{ maxWidth: 1200 }}>
         <Grid item sm={3}>
-          <List component="nav">
-            <ListItem button component={Link} to={`${match.url}/movies`}>
-              <ListItemIcon>
-                <MovieIcon />
-              </ListItemIcon>
-              <ListItemText primary="Movies" />
-            </ListItem>
-            <ListItem button component={Link} to={`${match.url}/rooms`}>
-              <ListItemIcon>
-                <LocalPlayIcon />
-              </ListItemIcon>
-              <ListItemText primary="Rooms" />
-            </ListItem>
-            <ListItem button component={Link} to={`${match.url}/experiences`}>
-              <ListItemIcon>
-                <StarIcon />
-              </ListItemIcon>
-              <ListItemText primary="Experiences" />
-            </ListItem>
-          </List>
+          <Paper className={classes.paper}>
+            <List component="nav">
+              <ListItem button component={Link} to={`${match.url}/movies`}>
+                <ListItemIcon>
+                  <MovieIcon />
+                </ListItemIcon>
+                <ListItemText primary="Movies" />
+              </ListItem>
+              <ListItem button component={Link} to={`${match.url}/rooms`}>
+                <ListItemIcon>
+                  <LocalPlayIcon />
+                </ListItemIcon>
+                <ListItemText primary="Rooms" />
+              </ListItem>
+              <ListItem button component={Link} to={`${match.url}/experiences`}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText primary="Experiences" />
+              </ListItem>
+            </List>
+          </Paper>
         </Grid>
         <Grid item sm={9}>
-          <Route exact path={`${match.path}`} render={() => {
-            return (
-              <div>
-                <Typography variant="h4" gutterBottom>Dashboard</Typography>
-                <Typography variant="body1">
-                  Welcome to your dashboard. Please click the navigational links on the left
-                  in order to configure the application.
-              </Typography>
-              </div>);
-            }} />
-          <Route exact path={`${match.path}/movies`} component={MovieList} />
-          <Route path={`${match.path}/movies/:movieID`} component={MovieEdit} />
-          <Route path={`${match.path}/rooms`} component={RoomList} />
-          <Route path={`${match.path}/experiences`} component={ExperienceList} />
+          <Paper className={classes.paper}>
+            <Route exact path={`${match.path}`} render={() => {
+              return (
+                <div>
+                  <Typography variant="h4" gutterBottom>Dashboard</Typography>
+                  <Typography variant="body1">
+                    Welcome to your dashboard. Please click the navigational links
+                    in order to configure the application.
+                </Typography>
+                </div>);
+              }} />
+            <Route exact path={`${match.path}/movies`} component={MovieList} />
+            <Route path={`${match.path}/movies/:movieID`} component={MovieEdit} />
+            <Route path={`${match.path}/rooms`} component={RoomList} />
+            <Route path={`${match.path}/experiences`} component={ExperienceList} />
+         </Paper>
         </Grid>
       </Grid>
     </div>
